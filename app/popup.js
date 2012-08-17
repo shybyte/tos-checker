1c514ad698914dee4a8388893318cfa994f08ae0
shybyte <ein@volloeko.de>
2012-08-17 09:53:27 +0200

    Removed "service rating" link text to fix issue #9

--- app/popup.js
+++ app/popup.js
@@ -64,7 +64,7 @@ jQuery(function () {
     function renderPopupHtml(name, longName, domain, verdict, ratingText, points, links) {
         var headerHtml = '<div class="modal-header">'
             + '<h3><a href="http://tos-dr.info/#' + name + '" target="_blank"><img src="images/tosdr-logo-32.png" alt="" class="pull-left" />'
-            + 'service rating</a></small>'
+            + '</a></small>'
             + '<button id="closeButton" data-dismiss="modal" class="close pull-right" type="button">×</button></h3></div>';
         var classHtml = '<div class="tosdr-rating"><label class="label ' + verdict + '">'
             + (verdict ? 'Class ' + verdict : 'No Class Yet') + '</label><p>' + ratingText + '</p></div>';
